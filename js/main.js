845a6450502b790fa4738b581a0791ca709957c1
shurupats <shurupats@gmail.com>
2022-12-11 21:09:31 +0300

    Удалила лишний код

--- js/main.js
+++ js/main.js
@@ -4,14 +4,6 @@ function changeColor(container){
 }
 
 function getColor(){
-    /*let color = Math.round(255.0*Math.random());
-    let r = color.toString(16);
-    color = Math.round(255.0*Math.random());
-    let g=color.toString(16);
-    color = Math.round(255.0*Math.random());
-    let d=color.toString(16);
-    color=r+g+d;
-    return color;*/
     let color = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
     return color;
 }
